48ff331058645eb0d2f791521b83aaabbc56bf46
Aarti chaudhari <aarti@ytel.co.in>
2018-04-27 11:18:42 +0530

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>My React Blog App</title>
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+
+</head>
+<body>
+
+<div id="app"></div><!-- actual wrapper for our react components. -->
+
+<script src="{{url('')}}/js/bundle.js"></script> <!-- webpack module bundler. -->
+</body>
+</html>
